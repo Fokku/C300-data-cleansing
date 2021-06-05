5309a60cdcc55481d6881d51a15343dfcf245232
Kai yuan <43857854+Fokku@users.noreply.github.com>
2021-06-05 15:58:06 +0800

    added tocsv.py

--- tocsv.py
+++ tocsv.py
@@ -1,16 +1,9 @@
 """ standard """
 import configparser as ConfigParser
-import json
-from random import randint
-import re
 import sys
 
 """ custom """
 from threatconnect import ThreatConnect
-from threatconnect.Config.FilterOperator import FilterOperator
-from threatconnect.Config.ResourceType import ResourceType
-from threatconnect.RequestObject import RequestObject
-from threatconnect.Config.IndicatorType import IndicatorType
 
 # configuration file
 config_file = "tc.conf"
@@ -35,16 +28,7 @@ tc.set_api_result_limit(api_result_limit)
 # Bulk Indicator object
 indicators = tc.bulk_indicators()
 
-owner = 'BruteForceBlocker Blocklist'
-
-# add a Filter and Post Filters
-
-try:
-    filter1 = indicators.add_filter()
-    filter1.add_owner(owner)
-except AttributeError as e:
-    print(e)
-    sys.exit(1)
+owner = "*"
 
 try:
     # retrieve the Indicators
@@ -53,19 +37,31 @@ except RuntimeError as e:
     print(e)
     sys.exit(1)
 
-# iterate through the results
+col = {
+    "Indicator": [],
+    "Type": [],
+    "Value": [],
+    "Organization": [],
+    "Rating": [],
+    "Confidence": [],
+    "DateAdded": [],
+    "LastModified": [],
+    "Description": [],
+    "Source": [],
+    "DNS": [],
+    "Whois": [],
+    "Active": [],
+    "Observations": [],
+    "Date Last Observed": [],
+    "False Positives": [],
+    "Date FP Last Reported": [],
+    "Tags": []
+}
+
 for indicator in indicators:
-    indicator.load_tags(indicator)
-    print(indicator.csv_header)
-    print(indicator.csv)
-    print()
-    print('')
-    # if the Indicator is a File Indicator or custom Indicator, print it out appropriately
-    if isinstance(indicator.indicator, dict):
-        for indicator_type, indicator_value in indicator.indicator.items():
-            print('{0}: {1}'.format(indicator_type, indicator_value))
-    else:
-        print(indicator.indicator)
-
-
-    print(indicator.tags)
+    indicator.load_attributes()
+    indicator.load_tags()
+    for attr_obj in indicator.attributes:
+        col["Type"].append(attr_obj.type)
+        col["Value"].append(attr_obj.value)
+        col[""]
